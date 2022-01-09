6237078e12ecaa09a14355b651a2e3b8199d3202
fh132 <fh132@CTS1C100618>
2022-01-09 18:01:07 -0300

    feat: adicionada o arquivo de rotas da aplicação

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { CreateUserController } from '@controllers/CreateUserController';
+import { Router } from 'express'
+
+const router = Router();
+
+const createUserController = new CreateUserController();
+router.post("/users", createUserController.handle);
+
+export { router }
