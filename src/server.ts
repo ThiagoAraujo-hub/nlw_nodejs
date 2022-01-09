536333a44017083337db1011505af32f7646e7ee
fh132 <fh132@CTS1C100618>
2022-01-09 18:03:30 -0300

    chore: algumas alterações que faltaram subir relacionadas ao roteamento da aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
-import express from 'express';
+import express, { json } from 'express';
 import "reflect-metadata";
 import "@database/index";
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({ message: "API NLW" })
-})
+app.use(json());
+app.use(router);
 
-app.post('/', (req, res) => {
-    res.json({ message: "API NLW método POST" })
-})
-
-app.listen(3000, () => console.log('Server is running'))
+app.listen(3000, () => console.log('Server is running\nWaiting for a request...'))
